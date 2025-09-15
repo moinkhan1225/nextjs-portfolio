6d7d87f758d645b2b98ebc012753fec62bc6bcaa
moinkhan1225 <moinkhanrobin1@gmail.com>
2025-09-15 23:47:49 +0530

    Updated All external Links to new tab

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -116,6 +116,8 @@ const HeroSection = () => {
 
             <Link
               href="https://drive.google.com/file/d/1V2iC8FvC11bE4jKu4XWc_JXCcN2tXrN1/view?pli=1" 
+               target="_blank"
+              rel="noopener noreferrer"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
